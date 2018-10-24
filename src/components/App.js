17f9e448d5208480b9704517e57f156b35856d22
Paul Green <paulgreen5555@gmail.com>
2018-10-24 23:14:01 +0300

    Node, NPM, React first steps.

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header.js';
+
+function App(){
+    return(
+        <div>
+            <Header/>
+            <div>APP</div>
+        </div>
+    )
+}
+
+export default App
