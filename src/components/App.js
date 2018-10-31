0eb0831cf83380790bb8584d2e6bbf819c22b1d3
Paul Green <paulgreen5555@gmail.com>
2018-10-31 23:34:58 +0300

    Add routing. Try to use react-boostrap

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
-import Page from './Page'
+import Header from './Header'
+import Footer from './Footer'
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
-function App() { return (<Page/>) }
+const Index = () => <h2>Home</h2>;
+const Projects = () => <h2>Projects</h2>;
+const Users = () => <h2>Users</h2>;
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <Header />
+                <Route path="/" exact component={Index} />
+                <Route path="/projects/" component={Projects} />
+                <Route path="/users/" component={Users} />
+                <Footer/>
+            </div>
+        </Router>
+    )
+}
 
 export default App
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import {Link} from "react-router-dom";
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
-function Header(){
-    return(
-        <div>Header</div>
+function Header() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/projects">Projects</Link>
+                </li>
+                <li>
+                    <Link to="/users">Users</Link>
+                </li>
+            </ul>
+        </div>
     )
 }
 
