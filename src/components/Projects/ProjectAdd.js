658db567ce0268364badefc9bf8d538161c4b0fc
Paul Green <paulgreen55555@gmail.com>
2018-11-14 23:26:16 +0300

    Work with add Project form. Move data from form to api through fetch.

--- src/components/Projects/ProjectAdd.js
+++ src/components/Projects/ProjectAdd.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import $ from 'jquery';
 
+const PROJECT_ADD_API = "http://localhost:1434/projects";
 export default class ProjectAdd extends Component {
     constructor(props) {
         super(props);
@@ -16,11 +18,30 @@ export default class ProjectAdd extends Component {
 
     handleChange(event) {
         const a = event.target.name;
-        this.setState({ [a] : event.target.value });
+        this.setState({ [a]: event.target.value });
     }
 
     handleSubmit(event) {
         event.preventDefault();
+
+        const formElement = document.getElementById("projectForm");
+
+        var fulldata = $(formElement).serializeArray();
+        var data = {};
+
+        $.map(fulldata, function(n, i){
+            data[n['name']] = n['value'];
+        });
+
+        fetch(PROJECT_ADD_API, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            
+            body: JSON.stringify(data)
+        });
 
         debugger;
 
@@ -28,7 +49,7 @@ export default class ProjectAdd extends Component {
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit} onChange={this.handleChange} >
+            <form id="projectForm" onSubmit={this.handleSubmit} onChange={this.handleChange} >
                 <label>
                     Project Name:
               <input name="Name" type="text" value={this.state.Name} />
@@ -38,9 +59,22 @@ export default class ProjectAdd extends Component {
               <input name="Description" type="text" value={this.state.Description} />
                 </label>
                 <label>
+                    OwnerId:
+              <input name="OwnerId" type="text" value={this.state.OwnerId} />
+                </label>
+                <label>
                     Start date:
               <input name="StartDate" type="date" value={this.state.StartDate} onChange={this.handleChange} />
                 </label>
+                <label>
+                    End date:
+              <input name="EndDate" type="date" value={this.state.EndDate} onChange={this.handleChange} />
+                </label>
+                <label>
+                    Close date:
+              <input name="CloseDate" type="date" value={this.state.CloseDate} onChange={this.handleChange} />
+                </label>
+
 
                 <input type="submit" value="Save" />
             </form>
